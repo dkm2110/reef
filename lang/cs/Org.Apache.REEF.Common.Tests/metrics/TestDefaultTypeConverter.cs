2deba7dc8b25dd5303157f95304f3315acbf9b7e
dhruv <dhruv@apache.org>
2016-07-10 21:12:27 -0700

    Changed APIs a bit

---
+++ lang/cs/Org.Apache.REEF.Common.Tests/metrics/TestDefaultTypeConverter.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Tang.Implementations.Tang;
+using Org.Apache.REEF.Common.metrics.Api;
+using Xunit;
+
+namespace Org.Apache.REEF.Common.Tests.metrics
+{
+    public sealed class TestDefaultTypeConverter
+    {
+        /// <summary>
+        /// Tests <see cref="DefaultTypeConverter"/>
+        /// </summary>
+        [Fact]
+        public void TestDefaultTypeConverterClass()
+        {
+            const float fValue = 12.13f;
+            const double dValue = 12.13;
+            const long longValue = 1213000;
+
+            ITypeConveter converter = TangFactory.GetTang().NewInjector().GetInstance<DefaultTypeConverter>();
+
+            Assert.Equal(converter.FromDouble(dValue), longValue);
+            Assert.Equal(converter.FromFloat(fValue), longValue);
+        }
+    }
+}
---
+++ lang/cs/Org.Apache.REEF.Common/metrics/Api/DefaultTypeConverter.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using Org.Apache.REEF.Tang.Annotations;
+
+namespace Org.Apache.REEF.Common.metrics.Api
+{
+    /// <summary>
+    /// Default implementation of <see cref="ITypeConveter"/>.
+    /// Multiplies float and double by 10^precision and then truncates.
+    /// </summary>
+    public class DefaultTypeConverter : ITypeConveter
+    {
+        private readonly int _precision;
+
+        [Inject]
+        private DefaultTypeConverter([Parameter(typeof(TypeConverterPrecisionParameter))] int precision)
+        {
+            _precision = precision;
+        }
+
+        /// <summary>
+        /// Converts double to long.
+        /// </summary>
+        /// <param name="value">double value</param>
+        /// <returns></returns>
+        public long FromDouble(double value)
+        {
+            return Convert.ToInt64(value * Math.Pow(10, _precision));
+        }
+
+        /// <summary>
+        /// Converts float to long.
+        /// </summary>
+        /// <param name="value">float value</param>
+        /// <returns></returns>
+        public long FromFloat(float value)
+        {
+            return Convert.ToInt64(value * Math.Pow(10, _precision));
+        }
+    }
+}
