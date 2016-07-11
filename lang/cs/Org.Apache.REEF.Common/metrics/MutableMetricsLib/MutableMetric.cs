01bb1c85c79057c64b3cdb1f3c5022bc8dc715f2
dhruv <dhruv@apache.org>
2016-07-10 19:41:39 -0700

    hi

---
+++ lang/cs/Org.Apache.REEF.Common/metrics/MutableMetricsLib/MutableMetric.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Org.Apache.REEF.Common.metrics.Api;
+
+namespace Org.Apache.REEF.Common.metrics.MutableMetricsLib
+{
+    public abstract class MutableMetric
+    {
+        private bool _changed = true;
+
+        public abstract void TakeSnapshot(IMetricsRecordBuilder recordBuilder, bool all);
+
+        public void TakeSnapshot(IMetricsRecordBuilder recordBuilder)
+        {
+            TakeSnapshot(recordBuilder, false);
+        }
+
+        protected void SetChanged()
+        {
+            _changed = true;
+        }
+
+        protected void ClearChanged()
+        {
+            _changed = false;
+        }
+
+        public bool Changed
+        {
+            get { return _changed; }
+        }
+    }
+}
